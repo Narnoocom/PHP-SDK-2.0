6dfee410bf9bc1195b629ac717a2641adb3a377f
James Wells <info@narnoo.com>
2014-03-05 14:52:08 +1000

    Replaces API keys with a config file
    Removed hardcoded API keys and created a single API config file.

--- demo/distributor-operator/albums.php
+++ demo/distributor-operator/albums.php
@@ -1,13 +1,7 @@
 <?php
+include '../../narnoo/config.php';
 include '../../narnoo/http/WebClient.php';
 include '../../narnoo/operatorconnect.php';
-
-$api_settings = array(
-	"API-KEY: 8WMVJYd5TF53o0xHTS",
-	"API-SECRET-KEY: MbG393WXRn75anUFuxgsw63G78ojI4rt",
-        "Authorization: c5KlUYOT2PCSWu1JaX0GCF4G12jVThR97CfA1Ypp"
-);
-
 
 $http_request = new Operatorconnect($api_settings);
 $response = $http_request->getAlbums(39); //pass operator ID
---
+++ narnoo/config.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * CONFIGURATAION KEYS FOR THE DEMO FILES
+ */
+$api_settings = array(
+	"API-KEY:8WMVJYd5TF53o0xHTS",
+	"API-SECRET-KEY: MbG393WXRn75anUFuxgsw63G78ojI4rt",
+        "Authorization: wCvya9ISHqbcFYiVWcuVDgMKWJII59cDZ8iI6vhx"
+);
