d6736af76b8e085a20cabce5546e11fe0d237492
James Wells <info@narnoo.com>
2014-03-24 13:34:19 +1000

    Removed Live API Keys

--- narnoo/config.php
+++ narnoo/config.php
@@ -3,7 +3,7 @@
  * CONFIGURATAION KEYS FOR THE DEMO FILES
  */
 $api_settings = array(
-	"API-KEY:8WMVJYd5TF53o0xHTS",
-	"API-SECRET-KEY: MbG393WXRn75anUFuxgsw63G78ojI4rt",
-        "Authorization: AJsIuyjqy8EVImFyoE3xTXeUishE1uEdzhibab13"
+	"API-KEY: { access-key }",
+	"API-SECRET-KEY: { secret-key }",
+        "Authorization: { token }"
 );
