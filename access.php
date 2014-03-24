89738228d9f8052833a4ececd68ddc52a6c5f4b0
James Wells <info@narnoo.com>
2014-03-24 16:32:28 +1000

    live keys
    These will be deleted

--- access.php
+++ access.php
@@ -4,13 +4,13 @@ include 'narnoo/http/WebClient.php';
 include 'narnoo/authenticate.php';
 
 $api_settings = array(
-	"API-KEY: { access-key }",
-	"API-SECRET-KEY: { secret-key }"
+	"API-KEY: 8WMVJYd5TF53o0xYZ2",
+	"API-SECRET-KEY: MbG393WXRn75anUFuxgsw63G78ojI3lI"
 );
 
 
 $http_request = new Authenticate($api_settings);
-$response = $http_request->access("{ access-key }","{ secret-key }");
+$response = $http_request->access("8WMVJYd5TF53o0xYZ2","MbG393WXRn75anUFuxgsw63G78ojI3lI");
 print_r($response);
 
 ?>
